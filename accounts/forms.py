59c3b081bf090a94fefb21d369bf58e98fe1a064
Roshen Abraham <rabrah99@Roshens-MacBook-Pro.local>
2022-04-07 18:39:43 -0400

    implemented profile feature

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,15 +6,29 @@ from accounts.models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     account_type = forms.ChoiceField(choices=((0, 'Public'), (1, 'Private')))
+    profile_pic = forms.ImageField(required=False)
+    skill_level = forms.ChoiceField(choices=((0, 'Beginner'), (1, 'Intermediate'), (2, 'Expert')))
+    bio = forms.CharField(widget=forms.Textarea)
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2", "first_name", "last_name")
+        widgets = {
+            'bio': forms.Textarea(attrs={
+                'rows': '5',
+                'cols': '90',
+                'max_length': '200',
+            }),
+        }
 
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         user.is_private = self.cleaned_data["account_type"]
+        # user.profile_pic = self.cleaned_data["profile_pic"]
+        # user.skill_level = self.cleaned_data["skill_level"]
+        # user.bio = self.cleaned_data["bio"]
+
         if commit:
             user.save()
         return user
@@ -24,6 +38,8 @@ class AddFundsCryptoForm(forms.Form):
     crypto_wallet_address = forms.CharField(max_length=36, min_length=25)
     amount_to_add = forms.DecimalField(decimal_places=2)
 
+
 class WithdrawForm(forms.Form):
     amount_to_withdraw = forms.DecimalField(decimal_places=2)
+
     
--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,6 @@ class CustomUser(AbstractUser):
     is_private = models.BooleanField(default=False)
     current_balance = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
     total_earnings = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
+    profile_pic = models.ImageField(blank=True)
+    skill_level = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
