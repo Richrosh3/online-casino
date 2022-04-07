7ad3c134d1a8d41bf14d863496a5c06e480c122e
Andrew Topmiller <andrewtopmiller@Andrews-MacBook-Pro.local>
2022-04-07 14:28:19 -0400

    create links to websites displaying current game sessions

--- menus/views.py
+++ menus/views.py
@@ -9,3 +9,28 @@ def index(request):
 @login_required
 def games(request):
     return render(request, 'menus/games.html')
+
+
+@login_required
+def current_poker_sessions(request):
+    return render(request, 'menus/sessions/poker.html')
+
+
+@login_required
+def current_blackjack_sessions(request):
+    return render(request, 'menus/sessions/blackjack.html')
+
+
+@login_required
+def current_craps_sessions(request):
+    return render(request, 'menus/sessions/craps.html')
+
+
+@login_required
+def current_roulette_sessions(request):
+    return render(request, 'menus/sessions/roulette.html')
+
+
+@login_required
+def current_slots_sessions(request):
+    return render(request, 'menus/sessions/slots.html')
