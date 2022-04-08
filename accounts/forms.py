075f7068030be3581a7af1d5cc7e914efa0bd6fd
Thomas Barton <tnbarton@umd.edu>
2022-04-07 23:21:04 -0400

    tnbarton adding tests

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from accounts.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    account_type = forms.ChoiceField(choices=((0, 'Public'), (1, 'Private')))
+    account_type = forms.ChoiceField(choices=((0, 'Public'), (1, 'Private')), required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = CustomUser
@@ -22,15 +24,22 @@ class CustomUserCreationForm(UserCreationForm):
 
 class AddFundsCryptoForm(forms.Form):
     crypto_wallet_address = forms.CharField(max_length=36, min_length=25)
-    amount_to_add = forms.DecimalField(decimal_places=2)
+    amount_to_add = forms.DecimalField(decimal_places=2, min_value=0)
 
 
 class AddFundsBankForm(forms.Form):
-    routing_number = forms.IntegerField()
-    account_number = forms.IntegerField()
-    amount_to_add = forms.DecimalField(decimal_places=2)
+    routing_number = forms.IntegerField(widget=forms.TextInput(
+        attrs={'pattern': '[0-9]{9}', 'title': "Routing number must be 9 digits"}))
+    account_number = forms.IntegerField(min_value=0, widget=forms.TextInput(
+        attrs={'pattern': '[0-9]*', 'title': "Enter a valid account number"}))
+    amount_to_add = forms.DecimalField(decimal_places=2, min_value=0)
+
+    def clean(self):
+        if len(str(self.cleaned_data.get('routing_number'))) != 9:
+            raise ValidationError("Routing number is not 9 digits")
+
+        return self.cleaned_data
 
 
 class WithdrawForm(forms.Form):
-    amount_to_withdraw = forms.DecimalField(decimal_places=2)
-    
+    amount_to_withdraw = forms.DecimalField(decimal_places=2, min_value=0)
---
+++ accounts/tests_features.py
@@ -0,0 +1,42 @@
+from django.contrib import auth
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import CustomUser
+
+
+class TestAccountFeatures(TestCase):
+    def test_create_user_and_logout_and_login(self):
+        self.assertFalse(CustomUser.objects.filter(username='user').exists())
+        signup_dict = {'username': 'user', 'email': 'user@umd.edu', 'password1': 'pass', 'password2': 'pass',
+                       'account_type': 0}
+        self.client.post(reverse('signup'), data=signup_dict)
+        self.assertTrue(CustomUser.objects.filter(username='user').exists())
+
+        self.client.get(reverse('logout'))
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+        login_dict = {'username': 'user', 'password': 'pass'}
+        self.client.post(reverse('login'), data=login_dict)
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+
+    def test_deposit_and_withdraw(self):
+        self.user = CustomUser.objects.create_user(username='user', password='pass', current_balance=1230.70)
+        self.client.login(username='user', password='pass')
+
+        bank_deposit_dict = {'routing_number': '123456789', 'account_number': '12345', 'amount_to_add': '125.00'}
+        self.client.post(reverse('add_funds_bank'), data=bank_deposit_dict)
+        self.user.refresh_from_db()
+        self.assertEquals(float(self.user.current_balance), 1230.70 + 125.00)
+
+        crypto_deposit_dict = {'crypto_wallet_address': '123456789123456789123456789', 'amount_to_add': '50.25'}
+        self.client.post(reverse('add_funds_crypto'), data=crypto_deposit_dict)
+        self.user.refresh_from_db()
+        self.assertEquals(float(self.user.current_balance), 1230.70 + 125.00 + 50.25)
+
+        withdraw_dict = {'amount_to_withdraw': '1000'}
+        self.client.post(reverse('withdraw_funds'), data=withdraw_dict)
+        self.user.refresh_from_db()
+        self.assertEquals(float(self.user.current_balance), round(1230.70 + 125.00 + 50.25 - 1000, 2))
