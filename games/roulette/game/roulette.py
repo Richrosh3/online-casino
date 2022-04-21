dea29720bca794f7a37e7f18808e0694f2f62907
Johnny Chiu <johnnychiu123@gmail.com>
2022-04-21 18:26:26 -0400

    create wheel and roulette basic functionality

--- games/roulette/game/roulette.py
+++ games/roulette/game/roulette.py
@@ -1,4 +1,39 @@
 from games.base import Game
+from uuid import UUID
+from games.roulette.game.wheel import Wheel
+
+from accounts.models import CustomUser
+
 
 class Roulette(Game):
 
+    def __init__(self, session_id: UUID):
+        super.__init__(session_id)
+
+        self.bets = {players: 0 for players in self.players}
+        self.wheel = Wheel()
+
+    def record_bet(self, player, amount):
+        self.bets[player] = amount
+
+    def reset(self):
+        self.wheel = Wheel()
+        for player in self.players:
+            self.bets[player] = 0
+
+    def remove_player(self, player: CustomUser):
+        if player in self.players:
+            self.bets.pop(player)
+            if len(self.players) == 0:
+                self.reset()
+
+    def add_player(self, player: CustomUser):
+        if player not in self.players:
+            self.players.add(player)
+            self.bets[player] = 0
+
+    def all_ready(self):
+        return all(self.bets.values())
+
+    def start_round(self):
+        self.wheel.roll()
