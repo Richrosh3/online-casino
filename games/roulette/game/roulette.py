594c8268b01b9813415c640b2e19108cbad85f20
Johnny Chiu <johnnychiu123@gmail.com>
2022-04-21 21:52:01 -0400

    Update variable names in roulette and RouletteUpdater place bet functionality

--- games/roulette/game/roulette.py
+++ games/roulette/game/roulette.py
@@ -10,30 +10,35 @@ class Roulette(Game):
     def __init__(self, session_id: UUID):
         super.__init__(session_id)
 
-        self.bets = {players: 0 for players in self.players}
+        self.bet_amount = {player: 0 for player in self.players}
+        self.bet_type = {player: None for player in self.players}
         self.wheel = Wheel()
 
-    def record_bet(self, player, amount):
-        self.bets[player] = amount
+    def record_bet(self, player, amount, bet_type: dict):
+        self.bet_amount[player] = amount
+        self.bet_type[player] = bet_type
 
     def reset(self):
         self.wheel = Wheel()
         for player in self.players:
-            self.bets[player] = 0
+            self.bet_amount[player] = 0
+            self.bet_type[player] = None
 
     def remove_player(self, player: CustomUser):
         if player in self.players:
-            self.bets.pop(player)
+            self.bet_amount.pop(player)
+            self.bet_type.pop(player)
             if len(self.players) == 0:
                 self.reset()
 
     def add_player(self, player: CustomUser):
         if player not in self.players:
             self.players.add(player)
-            self.bets[player] = 0
+            self.bet_amount[player] = 0
+            self.bet_type[player] = None
 
     def all_ready(self):
-        return all(self.bets.values())
+        return all(self.bet_amount.values())
 
     def start_round(self):
         self.wheel.roll()
