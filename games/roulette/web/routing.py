488ca838423071db08e406c3e813f2c0ddb2ee4e
Johnny Chiu <jchiu1@umd.edu>
2022-04-22 10:51:25 -0400

    Backend Roulette finalize functionalities

--- games/roulette/web/routing.py
+++ games/roulette/web/routing.py
@@ -1,2 +1,7 @@
+from django.urls import re_path
+from games.roulette.web import consumers
+
 websocket_urlpatterns = [
+    re_path(r'ws/roulette/(?P<session_id>[0-9a-f-]+)/$', consumers.RouletteConsumer.as_asgi())
+
 ]
